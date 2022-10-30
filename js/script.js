e6c7b32cb4b23d71701784d87a5ea6bfea02ffba
Antoni Woropiński <antoni.woropinski@gmail.com>
2022-10-30 15:14:12 +0100

    supplement of playerMove in JS

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
-printMessage(
-  "Zagrałem " + "kamień" + "! Jeśli Twój ruch to papier, to wygrywasz"
-);
-let computerMove = "kamień";
-printMessage(
-  "Zagrałem " + computerMove + "! Jeśli Twój ruch to papier, to wygrywasz!"
-);
-let playerMove = "papier";
-printMessage(
-  "Zagrałem " + playerMove + "! Jeśli Twój ruch to nozyce, to wygrywasz!"
-);
+let playerInput = prompt("Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.");
+
+console.log("Gracz wpisał: " + playerInput);
+
+let playerMove = "nieznany ruch";
+
+if (playerInput == "1") {
+  playerMove = "kamień";
+} else if (playerInput == "2") {
+  playerMove = "papier";
+} else if (playerInput == "3") {
+  playerMove = "nożyce";
+}
+
+printMessage("Twój ruch to: " + playerMove);
